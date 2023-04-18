d3742cf50282a65bafd2777f213f0051c168f6e9
johnntambwe <johnntambwe02@gmail.com>
2023-04-18 15:44:25 -0600

    Finishing with prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,52 @@
+using System.ComponentModel;
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        int grade = int.Parse(Console.ReadLine());
+        String letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        // Display the result or the grade
+        
+        Console.WriteLine($"Your grade is {letter}");
+
+        // verify if the student pass or not
+
+        int isPass = 70;
+
+        if (grade >= isPass)
+        {
+            Console.WriteLine("Congrats, You pass!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, work hard and try next time.");
+        }
     }
 }
