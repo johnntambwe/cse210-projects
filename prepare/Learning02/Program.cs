5b3aadd9d5c297fba06ce4a22468972be50de478
johnntambwe <johnntambwe02@gmail.com>
2023-05-02 13:10:45 -0600

    Finisging with abstraction part 1 learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // First Job
+
+        Job johnJob = new Job();
+        johnJob._company = "SanIluHpp Business";
+        johnJob._jobTitle = "Junior Admin";
+        johnJob._startYear = 2022;
+        johnJob._endYear = 2023;
+
+        //Second Job
+
+        Job johnJob1 = new Job();
+        johnJob1._company = "LDS Church";
+        johnJob1._jobTitle = "Executive Assistant";
+        johnJob1._startYear = 2020;
+        johnJob1._endYear = 2022;
+
+
+        Resume resume = new Resume();
+        resume._name = "John Ntambwe";
+        resume.jobs.Add(johnJob);
+        resume.jobs.Add(johnJob1);
+
+        resume.Display();
+       
     }
 }
